390493adb1a5264dafbf481fea60c0546b7cd518
wxie <williamxie00@gmail.com>
2025-12-05 20:31:36 -0700

    10x tracking weights

--- mujoco_playground/_src/locomotion/h12_skin/avoid.py
+++ mujoco_playground/_src/locomotion/h12_skin/avoid.py
@@ -56,10 +56,10 @@ def default_config() -> config_dict.ConfigDict:
       reward_config=config_dict.create(
           scales=config_dict.create(
               # Tracking rewards (active during MPC trajectory)
-              joint_pos_tracking=1.0,
+              joint_pos_tracking=10.0,
               joint_vel_tracking=0.5,
-              base_vel_tracking=1.0,
-              base_angvel_tracking=0.5,
+              base_vel_tracking=10.0,
+              base_angvel_tracking=5.0,
               torque_tracking=0.0, # 0.1, i dont think this even works
               
               # Recovery rewards (active after trajectory ends)
